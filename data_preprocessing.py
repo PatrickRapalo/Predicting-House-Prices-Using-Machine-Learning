02f76c56dc70c5fb8917d772e57ef333ee8ab482
Patrick Rapalo <patrick.rapalobach@gmail.com>
2025-02-07 00:31:09 -0500

    Added ames_house_data

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,1 +1,13 @@
 
+import pandas as pd
+import seaborn as sns
+
+
+def load_data(filepath="ames_house_data.csv"):
+    """Load dataset from CSV."""
+    return pd.read_csv(filepath)
+
+
+df = load_data()
+
+print(df.head())
